ecba46ad6166b5d369056af1508900a63181ff3e
aditi <Adishanu4u@gmail.com>
2025-04-19 16:34:25 -0700

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Aditi Agarwal on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
