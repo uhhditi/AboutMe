9565b129b8110897eec55e2812d5d334e4beea87
aditi <Adishanu4u@gmail.com>
2025-04-19 17:25:36 -0700

    added the content into my about me app

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -8,14 +8,110 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showText = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color(UIColor(red: 137/255, green: 204/255, blue: 4/255, alpha: 1.0))
+                .ignoresSafeArea()
+            ScrollView{
+                VStack() {
+                    Text("about    aditi")
+                        .font(.largeTitle)
+                    HStack{
+                        VStack(alignment: .leading, spacing: 20.0) {
+                            Image("ramen")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(10)
+                                .frame(width: 170, height: 170)
+                        }
+                        .padding()
+                        .background(Rectangle().foregroundColor(Color(red: 1.0, green: 0.8, blue: 0.85)))
+                        VStack(alignment: .leading, spacing: 20.0) {
+                            Image("ramen")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(10)
+                                .frame(width: 190, height: 170)
+                        }
+                        .padding()
+                        .background(Rectangle().foregroundColor(Color(red: 1.0, green: 0.8, blue: 0.85)))
+                        VStack(alignment: .leading, spacing: 20.0) {
+                            Image("ramen")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .cornerRadius(10)
+                                .frame(width: 170, height: 170)
+                        }
+                        .padding()
+                        .background(Rectangle().foregroundColor(Color(red: 1.0, green: 0.8, blue: 0.85)))
+                    }
+                    .padding(.horizontal)
+                    .border(Color.black, width: 2)
+                    VStack{
+                        Button(action: {
+                            showText.toggle()
+                        }) {
+                            Text("click   4   my  fav artists!")
+                                .foregroundColor(.black)
+                                .padding()
+                        }
+                        .background(Rectangle().foregroundColor(Color(red: 1.0, green: 0.8, blue: 0.85)))
+                        .border(Color.black, width: 2)
+                        
+                        if showText {
+                            HStack{
+                                Text("seventeen")
+                                    .font(.title2)
+                                    .foregroundColor(.black)
+                                Text("laufey")
+                                    .font(.title2)
+                                    .foregroundColor(.black)
+                                Text("arctic monkeys")
+                                    .font(.title2)
+                                    .foregroundColor(.black)
+                            }
+                                    }
+                        
+                    }
+                    Spacer()
+                    Text(" my   hobbies")
+                        .font(.title2)
+                    HStack{
+                        VStack{
+                            Image("concert")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 190, height: 170)
+                            Image("painting")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 190, height: 170)
+                        }
+                        VStack{
+                            Image("chocolate")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 190, height: 170)
+                            Image("cooking")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 190, height: 170)
+                        }
+                        
+                       
+                    }
+                    .padding()
+                    .background(Rectangle().foregroundColor(Color(red: 1.0, green: 0.8, blue: 0.85)))
+                    .border(Color.black, width: 2)
+                }
+                .padding()
+                
+            }
+            
+            
         }
-        .padding()
+        
     }
 }
 
